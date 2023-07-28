3b43af33779ebc83558a890eaf7f63fe8151aa7b
yongseopK <kk0@kakao.com>
2023-07-28 10:44:50 +0900

    core js - 스코프

---
+++ 1. core-js/6. 함수/10. let과 const.js
@@ -0,0 +1,32 @@
+let x;
+console.log(x);
+
+const y = 'hello';
+console.log(y);
+
+const COLOR_YELLOW = '#ff0';
+const COLOR_GREEN = '#0f0';
+const COLOR_BLACK = '#000';
+
+// $div.style.background = COLOR_YELLOW;
+// $span.style.background = COLOR_GREEN;
+// $div.style.background = COLOR_BLACK;
+
+
+const kim = {
+    name: '김철수',
+    age: 30
+};
+
+kim.name = '김마이클';
+// kim = {
+//     name: '김영희',
+//     age: 40
+// };
+
+console.log(kim);
+
+let a = 10;
+const b = 20;
+
+for (let i = 0; i < 10; i++) { }
