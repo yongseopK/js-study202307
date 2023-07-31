246eb498ef99210f9d3d64cea7a750fbb4feec3d
yongseopK <kk0@kakao.com>
2023-07-31 09:33:24 +0900

    점검문제 보완

--- 1. core-js/7.ES6/5. 점검문제1.js
+++ 1. core-js/7.ES6/5. 점검문제1.js
@@ -52,43 +52,51 @@ const traders = [
 // 연습 1: 2022년에 발생한 모든 거래를 찾아 
 //   거래자 정보(이름, 도시)를 배열에 매핑해주세요 
 const lastYearTrade = traders
-  .filter(t => t.year === 2022).map(t => {
-    return {
-      name: t.trader.name,
-      city: t.trader.city,
-    };
-  }).forEach(t => console.log(`이름 : ${t.name}\n도시: ${t.city}\n`));
+  .filter((t) => t.year === 2022)
+  .map((t) => t.trader)
+console.log(lastYearTrade);
 
 console.log('===============================================\n');
 
 // 연습 2: 거래자가 근무하는 모든 도시이름을 배열에 담아주세요.
-const workCity = traders
-  .map(t => t.trader)
-  .forEach(a => console.log(`${a.name}의 근무 도시 : ${a.city}`));
+// const workCity = traders
+//   .map(t => t.trader)
+//   .forEach(a => console.log(`${a.name}의 근무 도시 : ${a.city}`));
+
+const mappedCities = traders.map(trs => trs.trader.city);
+const cities = [...new Set(mappedCities)];
+console.log(cities);
 
 console.log('===============================================\n');
 
 // 연습 3: 대전에 근무하는 
 // 모든 거래자를 찾아 거래자정보를 배열에 매핑해주세요.
-const inDaejeonTrader = traders
+traders
   .filter(t => t.trader.city === '대전')
   .map(t => t.trader)
-  .forEach(t => console.log(t));
+  .forEach(trd => console.log(trd));
 
 console.log('===============================================\n');
 
 // 연습 4: 모든 거래자의 이름을 배열에 모아주세요.
 const traderName = traders
-  .map(t => t.trader.name)
-  .forEach(t => console.log(t));
-
+// .map(t => t.trader.name)
+// .forEach(t => console.log(t));
+const mappedNames = traders.map(t => t.trader.name);
+const names = [...new Set(mappedNames)];
+console.log(names);
 console.log('===============================================\n');
 
 // 연습 5: 서울에 사는 거래자의 모든 거래액의 총합 출력.
+// let total = 0;
+// const SeoulInTraderValue = traders
+//   .filter(t => t.trader.city === "서울")
+//   .map(t => t.value)
+//   .forEach(value => total += value);
+// console.log(total);
 
-const inSeoulInTraderValue = traders
+const total = traders
   .filter(t => t.trader.city === "서울")
-  .map(t => t.value)
-  .forEach(t => console.log(t));
-
+  .reduce((acc, curr) => acc + curr.value, 0);
+console.log(total);
 
