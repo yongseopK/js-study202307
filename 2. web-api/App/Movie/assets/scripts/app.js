728f43633985bfb01e1789ccd330a7821969b9cc
yongseopK <kk0@kakao.com>
2023-08-03 10:43:47 +0900

    web-api - 실전예제 2: 영화 추가 기능

--- 2. web-api/App/Movie/assets/scripts/app.js
+++ 2. web-api/App/Movie/assets/scripts/app.js
@@ -17,29 +17,80 @@ const $userInputs = [
 const [$titleInput, $imgUrlInput, $ratingInput] = $userInputs;
 const $entryTextSection = document.getElementById('entry-text');
 
+// 영화목록 ul태그
+const $movieList = document.getElementById('movie-list');
+
 const CLASS_VISIBLE = 'visible';
+
+// 영화 정보 목록 배열
+const movies = [];
+
+// ===== 유틸 함수, 일반함수 정의 ===== //
+
+/** 모든 인풋을 리셋하는 함수 */
+const clearMovieModalInput = () => {
+  $userInputs.forEach($input => $input.value = '');
+};
+
+/** 모달을 닫는 함수*/
+const closeAddModal = () => {
+  $backdrop.classList.remove(CLASS_VISIBLE);
+  $addMovieModal.classList.remove(CLASS_VISIBLE);
+
+  clearMovieModalInput();
+};
+
+/** 화면에 새로운 영화 정보를 렌더링하는 함수 */
+const renderNewMovie = ({ title, image, rating }) => {
+  const $newMovie = document.createElement('li');
+  $newMovie.classList.add('movie-element');
+
+  $newMovie.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${image}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating} / 5</p>
+    </div>
+  `;
+
+  $movieList.appendChild($newMovie);
+};
 
 // ===== 이벤트 핸들러 및 이벤트 바인딩 ===== //
 
-// Add movie버튼 클릭이벤트
+// 영화 추가버튼 기능을 수행하는 핸들러
+const addMovieHandler = e => {
+  const titleValue = $titleInput.value; //  제목입력란값
+  const imgUrlValue = $imgUrlInput.value; // 이미지 경로
+  const ratingValue = $ratingInput.value; // 평점 입력값
+
+  // 객체로 묶기
+  const newMovie = {
+    title: titleValue,
+    image: imgUrlValue,
+    rating: ratingValue
+  };
+  // console.log(newMovie);
+  movies.push(newMovie);
+  console.log(movies);
+
+  // 모달 닫기
+  closeAddModal();
+
+  // 화면에 입력한 영화정보 렌더링하기
+  renderNewMovie(newMovie);
+};
 
 // 영화 추가 모달창을 띄우는 핸들러
 const showMovieModalHandler = e => {
   $addMovieModal.classList.add(CLASS_VISIBLE);
   $backdrop.classList.add(CLASS_VISIBLE);
-
-  $titleInput.value = '';
-  $imgUrlInput.value = '';
 };
 
 
-const closeAddModal = () => {
-  $backdrop.classList.remove(CLASS_VISIBLE);
-  $addMovieModal.classList.remove(CLASS_VISIBLE);
 
-  $titleInput.value = '';
-  $imgUrlInput.value = '';
-};
 
 // 백트롭 영역을 클릭하면 모달이 닫히는 핸들러
 const backdropHandler = e => {
@@ -59,4 +110,7 @@ $backdrop.addEventListener('click', backdropHandler);
 
 // Add Movie모달 취소버튼 클릭이벤트
 $cancelAddMovieButton.addEventListener('click', closeMovieModalHandler);
+
+// Add Movie모달 추가버튼 클릭 이벤트
+$confirmAddMovieButton.addEventListener('click', addMovieHandler);
 
