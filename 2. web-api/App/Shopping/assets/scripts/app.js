38ceb109964eef96195e697cddfd2983c43891ce
yongseopK <kk0@kakao.com>
2023-08-04 13:33:44 +0900

    web-api - 실전예제3: 담기버튼 이벤트 this바인딩 문제 해결
    - ProductList 클래스 ul생성 분할
    - 이벤트 핸들러 등록 시 this가 잘못 바인딩 된 문제 해결

--- 2. web-api/App/Shopping/assets/scripts/app.js
+++ 2. web-api/App/Shopping/assets/scripts/app.js
@@ -20,6 +20,13 @@ class ProductItem {
   constructor(product) {
     this.product = product;
   }
+
+  // 담기버튼 클릭이벤트 핸들러
+  addToCartHandler() {
+    console.log('장바구니에 상품을 추가함!!');
+    console.log(this);
+    // 이 핸들러에서 누른 그 상품의 정보를 알야한다.
+  }
   render() {
     const $prod = document.createElement('li');
     $prod.classList.add('product-item');
@@ -34,35 +41,41 @@ class ProductItem {
             </div>
           </div>
         `;
+
+    const $addCartBtn = $prod.querySelector('button');
+    // $addCartBtn.addEventListener('click', this.addToCartHandler.bind(this));
+    $addCartBtn.addEventListener('click', () => this.addToCartHandler());
     return $prod;
   }
 }
 
-// 상품 목록에 대한 객체
-const productList = {
-  products: [
-    p1,
-    p2,
-    new Product(
-      '햄버거',
-      'https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG',
-      16900,
-      '꽈뜨로 맥씨멈 스테카 버거~'
-    ),
-    new Product(
-      '애플워치',
-      'https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510',
-      400000,
-      'APPLE watch 2세대 2022'
-    ),
-    new Product(
-      '맥북프로14',
-      'https://img.danawa.com/prod_img/500000/517/994/img/15994517_2.jpg?shrink=500:500',
-      2100000,
-      'APPLE 2021 맥북프로14 MKGR3KH/A M1 Max'
-    ),
-  ],
-
+// 한 개의 UL을 생성하는 클래스
+class ProductList {
+  constructor() {
+    // 상품들을 모아 놓은 배열
+    this.products = [
+      p1,
+      p2,
+      new Product(
+        '햄버거',
+        'https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG',
+        16900,
+        '꽈뜨로 맥씨멈 스테카 버거~'
+      ),
+      new Product(
+        '애플워치',
+        'https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510',
+        400000,
+        'APPLE watch 2세대 2022'
+      ),
+      new Product(
+        '맥북프로14',
+        'https://img.danawa.com/prod_img/500000/517/994/img/15994517_2.jpg?shrink=500:500',
+        2100000,
+        'APPLE 2021 맥북프로14 MKGR3KH/A M1 Max'
+      ),
+    ]
+  }
   render() {
     // console.log('render!!', this);
     const $app = document.getElementById('app');
@@ -75,7 +88,11 @@ const productList = {
     });
     $app.appendChild($prodList);
   }
-};
+}
+
+// 상품 목록에 대한 객체
+
 
 // 렌더링 명령
+const productList = new ProductList();
 productList.render();
